e5aee628eb3a6aeafdf5eacfa32d395947a4be4c
gimslab.com <gimslab.com@gmail.com>
2019-11-19 21:29:27 +0900

    actuator

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,9 @@ repositories {
 extra["springCloudVersion"] = "Hoxton.RC2"
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.cloud:spring-cloud-stream")
